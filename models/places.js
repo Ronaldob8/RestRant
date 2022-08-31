ce84f6441e820fad05a7c5ea79a5fc4cc750ef90
Ronaldo Barajas <rbarajas_8@hotmail.com>
2022-08-31 01:30:44 -0700

    pt 5 done

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports =[{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/zakaria-zayane-0uAVsDcyD0M-unsplash (1).jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/marissa-grootes-u2Lp8tXIcjw-unsplash.jpg'
+  }]
