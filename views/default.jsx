364dfa228ee59bea6740ceaa95959d2524337fea
Ronaldo Barajas <rbarajas_8@hotmail.com>
2022-08-31 01:41:44 -0700

    added nav bar

--- views/default.jsx
+++ views/default.jsx
@@ -6,9 +6,23 @@ function Def (html) {
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"></link>
-                <link rel='stylesheet' href="css/style.css"/>
+                <link rel='stylesheet' href="/style.css"/>
             </head>
             <body>
+            <nav>
+              <ul>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/places">Places</a>
+                </li>
+                <li>
+                  <a href="/places/new">Add Place</a>
+                </li>
+             </ul>
+            </nav>
+
                 {html.children}
             </body>
         </html>
